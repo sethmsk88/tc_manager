e1bbb0edcb9634cad966f5b3ecd41c97a8b46b5b
Seth Kerr <sethmsk88@yahoo.com>
2016-02-11 12:39:57 -0500

    Fix event_id issue with action buttons
    The event_id is not being extracted from the button id in all browsers.

--- scripts/manager.js
+++ scripts/manager.js
@@ -3,23 +3,23 @@ $(document).ready(function() {
 	/*************************/
 	/* Button Event Handlers */
 	/*************************/
-	$('#btn_addEvent').on('click', function(e){
+	$('#btn_addEvent').on('click', function() {
 		window.location.href = "./?page=add_event";
 	});
 
-	$('button.edit_button').on('click', function(e){
-		var caller_id = e.target.id;
-		var event_id = caller_id.match(/[0-9]+/);
-
+	$('button.edit_button').on('click', function() {
+		var regex_matchDigits = /\d+/;
+		var event_id = $(this).attr('id').match(regex_matchDigits)[0];
+		
 		window.location.href = "./?page=edit_event&id=" + event_id;
 	});
 
 	$('button.del_button').on('click', function(e){
 		e.preventDefault();
 
-		var caller_id = e.target.id;
-		var event_id = caller_id.match(/[0-9]+/)[0];
-
+		var regex_matchDigits = /\d+/;
+		var event_id = $(this).attr('id').match(regex_matchDigits)[0];
+		
 		$.ajax({
 			url: './content/act_del_event.php',
 			method: 'post',
@@ -36,7 +36,7 @@ $(document).ready(function() {
 				}
 				else if (response['action'] == 'delete') {
 					// Remove the event from the table that was just deleted
-					$('button.del_button[id="' + caller_id + '"]').closest('tr').remove();
+					$('button.del_button[id="' + $(this).attr('id') + '"]').closest('tr').remove();
 				}
 			}
 		});
